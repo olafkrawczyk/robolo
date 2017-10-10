11c5721b929a13b059caa833c062cb87c8b32519
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-10-10 15:48:43 +0200

    Initial commit

---
+++ robolo-app/src/main/java/com/ksm/robolo/roboloapp/RoboloAppApplication.java
@@ -0,0 +1,12 @@
+package com.ksm.robolo.roboloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboloAppApplication.class, args);
+	}
+}
---
+++ robolo-app/src/test/java/com/ksm/robolo/roboloapp/RoboloAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ksm.robolo.roboloapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoboloAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
